8d139242aef9d37e599c5fcd77f10e8ddd06f04c
beenehdev <beenehdev@gmail.com>
2026-01-13 22:12:04 -0800

    complete example functions meeting test spec

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function(...theArgs) {
+  let total = theArgs.reduce((acc, cur) => acc + cur);
+  return total;
+}
+
+const subtract = function(...theArgs) {
+  if (theArgs.length === 0) return 0;
+  if (theArgs.length === 1) return theArgs[0]
+
+	  let total = theArgs.reduce((acc, cur) => acc - cur);
+    return total;
 };
 
-const subtract = function() {
-	
+const sum = function(...theArgs) {
+  let flattenedArgs = theArgs.flat(2);
+  if (theArgs.length === 0) return 0;
+	let total = flattenedArgs.reduce((acc, cur) => acc + cur, 0);
+  return total;
 };
 
-const sum = function() {
-	
+const multiply = function(...theArgs) {
+  let flattenedArgs = theArgs.flat(2);
+  let total = flattenedArgs.reduce((acc, cur) => acc * cur);
+  return total;
 };
 
-const multiply = function() {
-
+const power = function(a, b) {
+	return a ** b
 };
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(n) {
+  let res = 1;
+	for (let i = 1; i <= n; i++) {
+    res *= i;
+  };
+  return res;
 };
 
 // Do not edit below this line
